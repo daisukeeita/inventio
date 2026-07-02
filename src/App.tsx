d7aec0fcf9afcd78e00a5610e602c86f5c40d2a4
javv <vinas.johnandrei@gmail.com>
2026-07-03 01:56:45 +0800

    deleted component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import LayoutSidebar from "@/components/layout/LayoutSidebar";
 import "./index.css";
 import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
-      <LayoutSidebar />
       <Button> CLICK ME! </Button>
     </>
   );
