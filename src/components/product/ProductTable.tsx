b7cf1eb7905f005cdd7c2e444d09408cf5c634df
javv <vinas.johnandrei@gmail.com>
2026-07-11 16:33:02 +0800

    updating the contents of component

--- src/components/product/ProductTable.tsx
+++ src/components/product/ProductTable.tsx
@@ -1,51 +1,68 @@
 import { useEffect, useState } from "react";
 import { Product, productApi, ResponseProduct } from "@/api/product";
+import { Button } from "@/components/ui/button";
 
 export default function ProductTable() {
-  const [productResponse, setProductResponse] = useState<ResponseProduct>(null);
+  const [productResponse, setProductResponse] =
+    useState<ResponseProduct | null>(null);
+  const [page, setPage] = useState<number>(0);
   const [error, setError] = useState<String | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setLoading(true);
+
     productApi
-      .getAll("0", "10")
-      .then(setProductResponse)
+      .getAll(page.toString(), "10")
+      .then((response) => {
+        setProductResponse(response);
+        setLoading(false);
+      })
       .catch((error) => setError(error.message));
-  }, []);
+  }, [page]);
 
   if (error) return <div> Error: {error}</div>;
 
-  if (!productResponse) return <div> Loading... </div>;
-
-  const products: Product[] = productResponse.data.content;
-
   return (
-    <>
+    <div>
       <h1> THIS IS THE PRODUCT TABLE </h1>
 
-      <table>
-        <thead>
-          <tr>
-            <th>SKU</th>
-            <th>NAME</th>
-            <th>SUPPLIER CODE</th>
-            <th>SUPPLIER NAME</th>
-            <th>BASE UNIT OF MEASURE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product, index) => {
-            return (
-              <tr key={index}>
-                <td>{product.sku}</td>
-                <td>{product.name}</td>
-                <td>{product.supplierCode}</td>
-                <td>{product.supplierName}</td>
-                <td>{product.baseUnitOfMeasure}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+      {loading ? (
+        <div> Loading... </div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>SKU</th>
+              <th>NAME</th>
+              <th>SUPPLIER CODE</th>
+              <th>SUPPLIER NAME</th>
+              <th>BASE UNIT OF MEASURE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productResponse?.data?.content.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <td>{product.sku}</td>
+                  <td>{product.name}</td>
+                  <td>{product.supplierCode}</td>
+                  <td>{product.supplierName}</td>
+                  <td>{product.baseUnitOfMeasure}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+
+      <Button
+        onClick={() => setPage((page: Number) => Math.max(Number(page) - 1, 0))}
+      >
+        Previous
+      </Button>
+      <span> {(page + 1).toString()} </span>
+      <Button onClick={() => setPage((page: number) => page + 1)}>Next</Button>
+    </div>
   );
 }
